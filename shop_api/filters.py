e699e7a6c0d2fdbb531bf87ae59928cd4bd06a59
Ilya Lazarev <ilya.v.lazarev@gmail.com>
2021-06-02 19:05:20 +0300

    product and review filters reworked, order filter added

--- shop_api/filters.py
+++ shop_api/filters.py
@@ -1,5 +1,5 @@
 from django_filters import rest_framework as filters
-from .models import Product, Review, Order
+from .models import Review, Order
 
 
 class ProductFilter(filters.FilterSet):
@@ -8,36 +8,23 @@ class ProductFilter(filters.FilterSet):
     name = filters.CharFilter(field_name="name", lookup_expr="icontains")
     description = filters.CharFilter(field_name="description", lookup_expr="icontains")
 
-    class Meta:
-        model = Product
-        fields = ["price", "name", "description"]
-
 
 class ReviewFilter(filters.FilterSet):
 
-    user = filters.NumberFilter(field_name="user", lookup_expr="exact")
-    product = filters.NumberFilter(field_name="product", lookup_expr="exact")
-    creation = filters.DateFilter()
-
     class Meta:
         model = Review
-        fields = ["user", "product", "creation"]
+        fields = ["user", "product", "created_at"]
 
 
 class OrderFilter(filters.FilterSet):
 
-    STATUS_CHOICES = [
-        ('NEW', 'Новый'),
-        ('IN_PROGRESS', 'В работе'),
-        ('DONE', 'Выполнен')
-    ]
-
-    status = filters.ChoiceFilter(choices=STATUS_CHOICES)
-    price = filters.RangeFilter()
-    created = filters.DateFilter()
-    updated = filters.DateFilter()
-    products = filters.MultipleChoiceFilter()  # разобраться
+    price = filters.RangeFilter(field_name='total_price')
+    positions = filters.NumberFilter(field_name='positions', method='products_filter')
 
     class Meta:
         model = Order
-        fields = ["status", "price", "created", "updated"]
+        fields = ["status", "created_at", "updated_at"]
+
+    def products_filter(self, queryset, name, value):
+        query = queryset.filter(positions__contains=[{"product":int(value)}])
+        return query
